cea6b46c497da0695fcacec2ee76dd53c62aeead
jkwill <469705283@qq.com>
2016-10-14 22:06:47 +0800

    delete the 分号

--- mysite/scutmocc/models.py
+++ mysite/scutmocc/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 
-
 # Create your models here.
 
 class MyUserManager(BaseUserManager):
@@ -49,7 +48,7 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
 class Person(models.Model):
     user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
     Nickname = models.CharField(max_length=10, null=False)
-    College = models.ForeignKey(    )
+    College = models.ForeignKey()
     Sex = models.BooleanField()
     Signature = models.CharField(max_length=100)
     Zc_Date = models.DateField(False, True)
@@ -60,69 +59,70 @@ class Person(models.Model):
     Sc_sum = models.IntegerField()
     Rank = models.IntegerField()
 
-#   Lession repo
+
+# Lession repo
 class LabelField(models.Model):
-    Lession={
+    Lession = {
         ('a', '项目'),
         ('b', '理论课'),
         ('c', '技能')
     }
-    Label_Kind = models.CharField(max_length=1, choices=Lession);
-    Label_Name = models.CharField(max_length=50);
+    Label_Kind = models.CharField(max_length=1, choices=Lession)
+    Label_Name = models.CharField(max_length=50)
 
 
 # College Field
 
 
 class CollegeField(models.Model):
-    College_Name = models.CharField(max_length=30);
-    College_Account = models.IntegerField();
+    College_Name = models.CharField(max_length=30)
+    College_Account = models.IntegerField()
 
 
 # Submit Lession
 
 
 class SubmitLes(models.Model):
-    Person_Id = models.ForeignKey(MyUser);
-    Label_Id = models.ForeignKey(LabelField);
-    Les_Name = models.CharField(max_length=60);
-    Les_Plan = models.CharField(max_length=200);
-    Les_Time = models.IntegerField();
-    Les_Way = models.CharField(max_length=200);
-    Les_Price = models.IntegerField();
-    Les_Merge = models.IntegerField();  # 0～3 社团 0~10
-    Les_Another = models.CharField(max_length=150);
-    Les_Term = models.IntegerField();
-    Les_Next = models.DateField();
-    Les_Status = models.BooleanField();
+    Person_Id = models.ForeignKey(MyUser)
+    Label_Id = models.ForeignKey(LabelField)
+    Les_Name = models.CharField(max_length=60)
+    Les_Plan = models.CharField(max_length=200)
+    Les_Time = models.IntegerField()
+    Les_Way = models.CharField(max_length=200)
+    Les_Price = models.IntegerField()
+    Les_Merge = models.IntegerField() # 0～3 社团 0~10
+    Les_Another = models.CharField(max_length=150)
+    Les_Term = models.IntegerField()
+    Les_Next = models.DateField()
+    Les_Status = models.BooleanField()
 
 
-# Comment Lession
+# Comment Lesson
 
 
 class CommentField(models.Model):
-    Les_Id = models.ForeignKey(SubmitLes);
-    Person_Id = models.ForeignKey(MyUser);
-    Comment = models.CharField(max_length=500);
+    Les_Id = models.ForeignKey(SubmitLes)
+    Person_Id = models.ForeignKey(MyUser)
+    Comment = models.CharField(max_length=500)
 
 
-# Answer Lession
+# Answer Lesson
 
 
 class AnswerField(models.Model):
-    Comment_Id = models.ForeignKey(CommentField);
-    Person_Id = models.ForeignKey(MyUser);
-    Answer = models.CharField(max_length=500);
+    Comment_Id = models.ForeignKey(CommentField)
+    Person_Id = models.ForeignKey(MyUser)
+    Answer = models.CharField(max_length=500)
 
-    #   Relation of Choice Lession
+    #   Relation of Choice Lesson
 
 
 class ChoiceLes(models.Model):
-    Les_Id = models.ForeignKey(SubmitLes);
-    Person = models.ForeignKey(MyUser);
-    Ch_Date = models.DateField();
-    End = models.BooleanField();
-    Les_Assess = models.CharField(max_length=500, null=True);
+    Les_Id = models.ForeignKey(SubmitLes)
+    Person = models.ForeignKey(MyUser)
+    Ch_Date = models.DateField()
+    End = models.BooleanField()
+    Les_Assess = models.CharField(max_length=500, null=True)
 
     #   Board
 
@@ -133,50 +133,49 @@ class Board(models.Model):
         ('b', '问题区'),
         ('c', '话题区')
     }
-    Board_type = models.CharField(max_length=1,choices=Field);
-    Gg_content = models.CharField(200);
-    Jrzt_sum = models.IntegerField();
-    Zrzt_sum = models.IntegerField();
-    Zt_sum = models.IntegerField();
+    Board_type = models.CharField(max_length=1, choices=Field)
+    Gg_content = models.CharField(200)
+    Jrzt_sum = models.IntegerField()
+    Zrzt_sum = models.IntegerField()
+    Zt_sum = models.IntegerField()
 
     #  Theme
 
 
 class Theme(models.Model):
-    Content = models.CharField();
-    Title = models.CharField();
-    Fb_date = models.DateTimeField();
-    Zjhf_date = models.DateTimeField();
+    Content = models.CharField()
+    Title = models.CharField()
+    Fb_date = models.DateTimeField()
+    Zjhf_date = models.DateTimeField()
     Zd = models.BooleanField()
-    Dz_sum = models.IntegerField();
-    Board_type = models.ForeignKey(Board);
-    Fbr_id = models.ForeignKey(MyUser);
-    Yd_sum = models.IntegerField();
-    Legal = models.BooleanField();
-    Sc_sum = models.IntegerField();
+    Dz_sum = models.IntegerField()
+    Board_type = models.ForeignKey(Board)
+    Fbr_id = models.ForeignKey(MyUser)
+    Yd_sum = models.IntegerField()
+    Legal = models.BooleanField()
+    Sc_sum = models.IntegerField()
 
     # BBS answer
 
 
 class ThemeAnswer(models.Model):
-    Hfr_Id = models.ForeignKey(MyUser);
-    Lc_no = models.IntegerField();
-    Fb_date = models.DateTimeField();
-    Theme_Id = models.ForeignKey(Theme);
-    Dz_sum = models.IntegerField();
-    Legal = models.BooleanField();
+    Hfr_Id = models.ForeignKey(MyUser)
+    Lc_no = models.IntegerField()
+    Fb_date = models.DateTimeField()
+    Theme_Id = models.ForeignKey(Theme)
+    Dz_sum = models.IntegerField()
+    Legal = models.BooleanField()
 
     # Collect Theme
 
 
 class CollectTheme(models.Model):
-    Yh_Id = models.ForeignKey(MyUser);
-    Theme_Id = models.ForeignKey(Theme);
+    Yh_Id = models.ForeignKey(MyUser)
+    Theme_Id = models.ForeignKey(Theme)
 
     # Pay attention
 
 
 class Attention(models):
-    Fs_Id = models.ForeignKey(MyUser);
-    Ox_Id = models.ForeignKey(MyUser);
-
+    Fs_Id = models.ForeignKey(MyUser)
+    Ox_Id = models.ForeignKey(MyUser)
